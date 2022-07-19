6f34a090722bba03321d23d13e43d1c1ae90b012
William Nery <william.nery.pereira@gmail.com>
2022-07-19 18:23:55 -0300

    Evento de click

--- app/src/main/java/com/williamnery/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/williamnery/gastoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.williamnery.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.williamnery.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,8 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate) {
+            calculate()
+        }
     }
 
     private fun calculate() {
+        // Toast notification
+        Toast.makeText(this, "Fui clicado!", Toast.LENGTH_SHORT).show()
     }
 }
