6964476650a70d8fd2167c500ba96d5e3d1c343c
William Nery <william.nery.pereira@gmail.com>
2022-07-19 18:02:24 -0300

    ViewBinding e padronização dos campos

--- app/src/main/java/com/williamnery/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/williamnery/gastoviagem/MainActivity.kt
@@ -2,10 +2,18 @@ package com.williamnery.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.williamnery.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun calculate() {
     }
 }
